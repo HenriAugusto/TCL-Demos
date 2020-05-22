59caaff089f8ed5f6e1e9736ad4d99baa5e0ff44
HenriAugusto <msndohenri@hotmail.com>
2020-05-22 00:18:20 -0300

    Init Repo - Canvas Tooltip System

---
+++ Tooltips/Tooltips.tcl
@@ -0,0 +1,150 @@
+proc dbg {str} {
+	set curr [.console cget -text]
+	if {$curr eq ""} {
+		.console configure -text "$str"
+	} else {
+		.console configure -text "$curr\n$str"
+	}
+}
+
+proc dbg_clear {} {
+	.console configure -text ""
+}
+
+# Tooltips can have as many tags as you need.
+# The first tag should always be a unique identifier for the tooltip!
+# all tooltips will receive a "tooltip" tag
+#
+# The colors are passed TK style using
+#	-textcolor
+#	-bkg
+#	-outline
+# see:
+# https://stackoverflow.com/questions/29150599/call-tcl-proc-with-named-arguments
+proc create_tooltip {mytoplevel x y text tags args} {
+	set uniqueTag [lindex $tags 0]
+	set textTags "$tags tooltip"
+	set bkgTags "$tags tooltipBkg"
+
+	array set optional [list -textcolor "blue" -bkg "white" -outline "blue" -anchor center {*}$args]
+	set textcolor $optional(-textcolor)
+	set bkg $optional(-bkg)
+	set outline $optional(-outline)
+
+    set textId [$mytoplevel create text $x $y -text $text -tags $textTags -fill $textcolor]
+
+    # the tag arg passed to bbox should be a single tag or a tag expression
+    # set boundingBox [$mytoplevel bbox {tooltip || tooltipBkg}]
+
+    set boundingBox [$mytoplevel bbox $textId]
+    set bkgId [$mytoplevel create rect $boundingBox -fill $bkg -outline $outline -tags $bkgTags]
+    $mytoplevel lower "tooltipBkg"
+
+    if {$optional(-anchor) eq "left" || $optional(-anchor) eq "right"} {
+    	set tooltip_w [expr {[lindex $boundingBox 2]-[lindex $boundingBox 0]}]
+    	set sign [expr {$optional(-anchor) eq "left" ? 1 : -1}]
+    	.c move $textId [expr {$sign*$tooltip_w/2}] 0
+    	.c move $bkgId [expr {$sign*$tooltip_w/2}] 0
+    }
+
+    button .test.del_btn_$textId -text "delete $text using textId: $textId" -command ".c delete $textId;.c delete $bkgId;"
+    button .test.del_btn_tag_$textId -text "delete $text using unique tags: $uniqueTag" -command ".c delete $uniqueTag"
+
+    grid .test.del_btn_$textId -row [expr {$textId+1}] -column 0
+    grid .test.del_btn_tag_$textId -row [expr {$textId+1}] -column 1
+
+}
+
+proc initialize_windows {} {
+	# window that will display the tooltips
+	canvas .c -background #ffffff
+
+	label .console
+	.console configure -text ""
+	bind . <Key-Escape> exit
+
+
+	pack .c
+	pack .console
+	wm geometry . +0+0
+
+	initialize_test_window
+	initialize_tooltips
+
+	set w [.c cget -width]
+	set h [.c cget -height]
+}
+
+proc initialize_tooltips {} {
+	set w [.c cget -width]
+	set h [.c cget -height]
+
+	# left ones
+	create_tooltip .c [expr $w/2-125] 50  left_A {left_A_TAG left}
+	create_tooltip .c [expr $w/2-125] 100 left_B {left_B_TAG left}
+	create_tooltip .c [expr $w/2-125] 150 left_C {left_C_TAG left} -outline #00ff00
+	create_tooltip .c [expr $w/2-125] 200 left_D {left_D_TAG left} -textcolor red
+	create_tooltip .c [expr $w/2-125] 250 left_E {tooltip_E_TAG left} -bkg black -textcolor white -outline red
+
+	# right ones
+	create_tooltip .c [expr $w/2+125] 50  "right_A" "right_A_TAG right"
+	create_tooltip .c [expr $w/2+125] 100 "right_B" "right_B_TAG right"
+	create_tooltip .c [expr $w/2+125] 150 "right_C" "right_C_TAG right"
+	create_tooltip .c [expr $w/2+125] 200 "right_D" "right_D_TAG right"
+	create_tooltip .c [expr $w/2+125] 250 "right_E" "right_E_TAG right"
+
+	# middle ones
+
+	.c create line [expr $w/2] 0 [expr $w/2] $h -fill black
+	create_tooltip .c [expr $w/2] [expr $h/2+50] "anchor_right" "anchor_right_TAG" -anchor right
+	create_tooltip .c [expr $w/2] [expr $h/2] "anchor_center" "anchor_center_TAG" -anchor center
+	create_tooltip .c [expr $w/2] [expr $h/2-50] "anchor_left" "anchor_left_TAG" -anchor left
+
+	.c bind "tooltip" <ButtonPress> {dbg "Clicked on tooltip (%x, %y)"}
+}
+
+proc initialize_test_window {} {
+	#window with the commands
+	toplevel .test
+	bind .test <Key-Escape> exit
+	#button .test.restart -text "restart" -command {initialize_tooltips}
+	button .test.restart -text "restart" -command {restart}
+
+	# needed to get correct info
+	update
+
+	wm geometry .test +[winfo width .]+0
+
+	button .test.del_left -text "delete left" -command {delete_tooltips_with_tag "left"}
+	button .test.del_right -text "delete right" -command {delete_tooltips_with_tag "right"}
+
+	label .test.escape -text "Press Escape to quit"
+
+	grid .test.restart -row 0 -column 0
+	grid .test.escape -row 0 -column 1
+	grid .test.del_left -row 1 -column 0
+	grid .test.del_right -row 1 -column 1
+}
+
+proc delete_tooltips_with_tag {tag} {
+	set results [.c find withtag $tag]
+	foreach i $results {
+		.c delete $i
+	}
+}
+
+proc print_info {} {
+
+}
+
+proc restart {} {
+	.c delete all
+	destroy .test
+	dbg_clear
+	initialize_test_window
+	initialize_tooltips
+	#focus .test
+	raise .
+}
+
+initialize_windows
---
+++ readme.md
@@ -0,0 +1,6 @@
+# TCL Demos
+
+Those are small TK/TCL demonstrations and samples on how to do stuff.
+
+- Tooltips
+	- showcase a simple tooltip system that works on canvases
